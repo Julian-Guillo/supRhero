3ccfd17355bc39081948ea4593384cee201093bb
Julián Guilló <julianleioa@gmail.com>
2025-07-13 14:12:06 +0200

    Extend superhero list

--- R/data.R
+++ R/data.R
@@ -3,4 +3,31 @@
 #' A character vector containing names of superheroes.
 #'
 #' @export
-superheroes <- c("Spider-Man", "Iron Man", "Wonder Woman", "Batman", "Black Panther", "Captain Marvel", "Thor", "Hulk")
+superheroes <- c(
+  "Spiderman", "Iron Man", "Wonder Woman", "Batman", "Black Panther", "Captain Marvel", "Thor", "Hulk",
+  "Superman", "The Flash", "Green Lantern", "Aquaman", "Cyborg", "Martian Manhunter", "Green Arrow",
+  "Doctor Strange", "Wolverine", "Deadpool", "Scarlet Witch", "Vision", "Ant-Man", "Wasp", "Star-Lord",
+  "Gamora", "Drax", "Rocket Raccoon", "Groot", "Nick Fury", "Hawkeye", "Falcon", "Winter Soldier",
+  "Shazam", "Batgirl", "Supergirl", "Nightwing", "Red Hood", "Robin", "Beast Boy", "Raven", "Starfire",
+  "Blue Beetle", "Black Canary", "Zatanna", "Constantine", "Etrigan", "Plastic Man", "Booster Gold",
+  "The Atom", "Vixen", "Firestorm", "Moon Knight", "She-Hulk", "Luke Cage", "Iron Fist", "Jessica Jones",
+  "Punisher", "Blade", "Ghost Rider", "Silver Surfer", "Nova", "Sentry", "Ms. Marvel", "Kate Bishop",
+  "Echo", "Shuri", "Namor", "America Chavez", "Moon Girl", "Devil Dinosaur", "Hellcat", "Daredevil",
+  "Elsa Bloodstone", "Beta Ray Bill", "Magik", "Iceman", "Angel", "Psylocke", "Storm", "Jean Grey",
+  "Cyclops", "Colossus", "Shadowcat", "Rogue", "Gambit", "Professor X", "Cable", "Domino", "X-23",
+  "Sabra", "Bishop", "Cloak", "Dagger", "Captain Britain", "Black Knight", "Hercules", "Man-Thing",
+  "Howard the Duck", "Quasar", "Darkhawk", "Speedball", "Justice", "Firestar", "Songbird", "Mach-X",
+  "Spectrum", "Photon", "Blue Marvel", "Monica Rambeau", "Agent 13", "Maria Hill", "War Machine",
+  "Rescue", "Pepper Potts", "Happy Hogan", "Yondu", "Ronan the Accuser", "Korg", "Miek", "Hela",
+  "Valkyrie", "Sif", "Loki", "Throg", "Red She-Hulk", "Spider-Woman", "Spider-Gwen", "Miles Morales",
+  "Ben Reilly", "Spider-Ham", "Silk", "Ironheart", "Amadeus Cho", "Brawn", "Kid Loki", "Young Thor",
+  "Young Avengers Hulkling", "Wiccan", "Patriot", "Speed", "Stature", "Ms. America", "Iron Lad",
+  "Kamala Khan", "Squirrel Girl", "Doop", "Glob Herman", "Armor", "Pixie", "Surge", "Dust", "Rockslide",
+  "Elixir", "Prodigy", "Hellion", "X-Man", "Blink", "Morph", "Marrow", "Forge", "Strong Guy", "Polaris",
+  "Havok", "Longshot", "Dazzler", "Boom-Boom", "Cannonball", "Sunspot", "Warpath", "Domino", "Shatterstar",
+  "Feral", "Rictor", "Multiple Man", "Siryn", "Mimic", "Northstar", "Aurora", "Guardian", "Snowbird",
+  "Sasquatch", "Puck", "Talisman", "Manifold", "Armor", "Lady Sif", "Beta Ray Bill", "Thena", "Ikaris",
+  "Makkari", "Sprite", "Gilgamesh", "Kingo", "Phastos", "Ajak", "Moonstone", "Songbird", "Baron Zemo",
+  "Red Guardian", "Yelena Belova", "Taskmaster", "U.S. Agent", "Machine Man", "Vision (White)",
+  "Quicksilver", "Black Widow", "White Tiger", "Power Man", "Nova (Sam Alexander)", "Nova (Richard Rider)"
+)
